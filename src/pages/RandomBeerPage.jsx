58db01ed579a6a8eb5571b7a788cc4cb7476908c
EugeBr <eugebordenave@gmail.com>
2022-07-04 16:53:43 +0200

    Iteration 5

--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import Header from '../components/Header'
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import axios from 'axios';
+import { useParams } from 'react-router-dom'
+import SingleBeer from '../components/SingleBeer';
 
-export default function RandomBeerPage(){
+export default function RandomBeerPage() {
+    const [randomBeer, setRandomBeer] = useState(null);
+    useEffect(() => {
+        const fetchBeer = async () => {
+            try {
+                const response = await axios.get('https://ih-beers-api2.herokuapp.com/beers/random');
+                setRandomBeer(response.data)
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchBeer();
+    }, [])
+
     return (
         <>
-        <Header />
-        <h1>Random Beer</h1>
+            <Header />
+            {
+                randomBeer ? <SingleBeer {...randomBeer} /> : <p>Loading random beer...</p>
+            }
         </>
     )
 }
